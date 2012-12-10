f37444c59f7125c5f22a93f22ad2d0654ccb81f8
ouyangtao404 <ouyangtao404@qq.com>
2012-12-10 17:44:39 +0800

    个人信息&分享

---
+++ _includes/share.php
@@ -0,0 +1,13 @@
+<div class="blog-share">
+	<!-- TianJi Button BEGIN -->
+	<div id="tianji_style_32x32">
+		<a title="分享到新浪微博" class="tianji_button_tsina"></a>
+		<a title="分享到QQ空间" class="tianji_button_qzone"></a>
+		<a title="分享到腾讯微博" class="tianji_button_tqq"></a>
+		<a title="分享到开心网" class="tianji_button_kaixin001"></a>
+		<a title="分享到人人网" class="tianji_button_renren"></a>
+		<a href="http://tj.see-say.com/share.asp" class="tianji tianji_txt jtico jtico_tianji" target="_blank"></a>
+	</div>
+	<script type="text/javascript" src="http://tj.see-say.com/code/jia.js" charset=utf-8></script>
+	<!-- TianJi Button END -->
+</div>
--- _includes/side-bar.php
+++ _includes/side-bar.php
@@ -2,8 +2,13 @@
 <aside id="sidebar">
 	
 	<ul>
+		
+		<li class="block me">
+			<iframe width="260" height="100" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=260&height=100&fansRow=2&ptype=1&speed=0&skin=1&isTitle=1&noborder=0&isWeibo=0&isFans=0&uid=1629780185&verifier=81cd8ab8&colors=F3E4C8,F3E4C8,444444,A83E2A,56fc33&dpc=1"></iframe>
+		</li>
+		
 		<li class="block" style="">
-    		<h4 class="heading">blog information</h4>
+    		<h4 class="heading">博客信息</h4>
     		<span>博客字数：{{ content | number_of_words }}</span><br/>
 			<span>最后更新时间：{{ site.time | date_to_long_string }}</span><br/>
 		</li>
